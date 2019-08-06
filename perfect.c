fd8ecbd45efb94f01796e43f83e4ce809e1d8c0d
RamyLerner <san85@walla.com>
2019-08-06 17:12:36 +0300

    Add files via upload

---
+++ perfect.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+
+int perfect(int n){
+	int i, sum=0;
+	for (i=1; i<=sqrt(n); i++){
+		if (n%i == 0){
+			sum += i;
+			if (i != sqrt(n))
+				sum += (n/i);
+		}
+	}
+	if (sum == n*2)
+		return 1;
+	else
+		return 0;
+}
+
+int main(){
+	int n;	
+	do{
+		printf("enter a positiv number: ");
+		scanf("%d",&n);
+		if (n<0)
+			printf("oops, that's negative!\n");
+	} while (n<0);
+	printf("\n%d\n",perfect(n));
+		
+}
