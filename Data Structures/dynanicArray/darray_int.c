18be2e20165325e1e4c76ce54c3083b77d1eeaf7
RamyLerner <san85@walla.com>
2019-08-21 07:36:14 +0300

    Add files via upload

---
+++ Data Structures/dynanicArray/darray_int.c
@@ -0,0 +1,115 @@
+#include<stdio.h>
+#include"darray_int.h"
+
+struct darray{
+	int* arr;
+	size_t initialCap;
+	size_t capacity;
+	size_t size;
+};
+
+
+AdtStatus darrayCreate(darray **dArr, size_t initial_capacity){
+	int* arr;
+
+	*dArr = (darray*)malloc(sizeof(darray));
+	if (dArr == NULL)
+		return AllocationError;
+	arr = (int*)malloc(sizeof(int) * initial_capacity);
+	if (arr == NULL){
+		free(*dArr);
+		return AllocationError;
+	}
+	(*dArr)->arr = arr;
+	(*dArr)->initialCap = initial_capacity;
+	(*dArr)->capacity = initial_capacity; 
+	(*dArr)->size = 0;
+	return OK;
+}
+
+
+AdtStatus darrayDestroy(darray *dArr){
+	if (dArr != NULL)
+		free(dArr -> arr);
+	free (dArr);
+	return OK;
+}
+
+
+AdtStatus   darrayAdd(darray *dArr,  int  _item){
+	darray* temp;
+	if (dArr == NULL)
+		return NullPointerError;
+	if (dArr->size == dArr->capacity){
+		temp = dArr;
+		dArr = (darray*)realloc(dArr, dArr->capacity * 2 * sizeof(int));
+		if (dArr == NULL){
+			dArr = temp;
+			return AllocationError;
+		}
+		dArr->capacity *= 2;
+		printf("reallocated\n");
+	}
+	dArr->arr[dArr->size] = _item; 
+	dArr->size++;
+	return OK;
+}
+
+
+AdtStatus   darrayDelete(darray *dArr,  int* _item){
+	darray* temp;
+	*_item = dArr->arr[dArr->size - 1];
+	if (dArr == NULL)
+		return NullPointerError;
+	if (dArr->capacity > dArr->initialCap && dArr->size <= dArr->capacity * 3 / 8){
+		temp = dArr;
+		dArr = realloc(dArr, dArr->capacity / 2 * sizeof(int));
+		if (dArr == NULL){
+			dArr = temp;
+			return AllocationError;
+		}
+		dArr->capacity /= 2;
+		printf("reallocated\n");
+	}
+	dArr->size--;
+	return OK;
+}
+
+
+AdtStatus   darrayGet(darray *dArr, size_t _index, int *_item){
+	if (dArr == NULL)
+		return NullPointerError;
+	if (_index >= dArr->size)
+		return IndexError;
+	*_item = dArr->arr[_index];
+	return OK;
+}
+
+
+AdtStatus   darraySet(darray *dArr, size_t _index, int  _item){
+	if (dArr == NULL)
+		return NullPointerError;
+	if (_index >= dArr->size)
+		return IndexError;
+	dArr->arr[_index] = _item;
+	return OK;
+}
+
+
+AdtStatus   darrayItemsNum(darray *dArr, int *_numOfItems){
+	if (dArr == NULL)
+		return NullPointerError;
+	*_numOfItems = dArr->size;
+	return OK;
+}
+
+
+AdtStatus darrayPrint(darray *dArr){
+	int i;
+	if (dArr == NULL)
+		return NullPointerError;
+	for (i=0; i<dArr->size; i++)
+		printf("%d ",dArr->arr[i]);
+	putchar('\n');
+	return OK;
+}
