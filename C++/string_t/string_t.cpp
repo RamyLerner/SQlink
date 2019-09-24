f28c8a49aa96614ca23026e7ba3c6e95c2481f25
SQRT <san85@walla.com>
2019-09-24 19:28:31 +0300

    compiled

--- C++/string_t/string_t.cpp
+++ C++/string_t/string_t.cpp
@@ -5,7 +5,7 @@
 
 int String_t::numOfStrings = 0;
 int String_t::caseSens = 0;
-int String_t::defaultCapacity = 16;
+size_t String_t::defaultCapacity = 16;
 
 String_t::String_t()
 {
@@ -48,184 +48,191 @@ String_t &String_t::operator=(const String_t &st)
 {
 	if (this != &st)
 	{
-		setString(st.String);
+		setString(st.string);
 	}
 	return *this;
 }
 
-void setString(const char *s)
+void String_t::setString(const char *s)
 {
+
+	unsigned int len;
+	if (!s)
 	{
-		int len;
-		if (!s)
+		string = createEmpty();
+		length = 0;
+	}
+	else
+	{
+		len = strlen(s);
+		if (len + 1 > capacity)
 		{
-			string = createEmpty();
-			length = 0;
+			delete[] string;
+			string = createFrom(s);
+			capacity = len + 1;
 		}
 		else
-		{
-			len = strlen(s);
-			if (len + 1 > capacity)
-			{
-				delete string[];
-				string = createFrom(s);
-				capacity = len + 1;
-			}
-			else
-				strcpy(s, string);
+			strcpy(string, s);
 
-			length = len;
-		}
+		length = len;
 	}
+}
 
-	int String_t::compareString(const String_t &st) const
+int String_t::compareString(const String_t &st) const
+{
+	int res;
+	if (caseSens)
+		res = strcmp(string, st.string);
+	else
+		res = strcasecmp(string, st.string);
+	if (!res)
+		return 0;
+	return res < 0 ? 1 : 2;
+}
+
+void String_t::toLowerString()
+{
+	int i = 0;
+	while (string[i])
 	{
-		int res;
-		if (caseSens)
-			res = strcmp(string, st.string);
-		else
-			res = strcasecmp(string, st.string);
-		if (!res)
-			return 0;
-		return res < 0 ? 1 : 2;
+		string[i] = tolower(string[i]);
+		i++;
 	}
+}
 
-	void String_t::toLowerString()
+void String_t::toUpperString()
+{
+	int i = 0;
+	while (string[i])
 	{
-		int i = 0;
-		while (string[i])
-		{
-			string[i] = tolower(string[i]);
-			i++;
-		}
+		string[i] = toupper(string[i]);
+		i++;
 	}
+}
 
-	void String_t::toUpperString()
-	{
-		int i = 0;
-		while (string[i])
-		{
-			string[i] = toupper(string[i]);
-			i++;
-		}
-	}
+void String_t::appendAndSet(const char *s1, const char *s2)
+{
+	int len1 = strlen(s1), len2 = strlen(s2);
+	char* temp = new char[len1 + len2 + 1];
+	strcpy(temp, s1);
+	strcat(temp, s2);
+	setString(temp);
+	delete[] temp;
+}
 
-	void appendAndSet(const char *s1, const char *s2)
-	{
-		int len1 = strlen(s1), len2 = strlen(s2);
-		char temp[len1 + len2 + 1];
-		strcpy(temp, s1);
-		strcat(temp, s2);
-		setString(temp);
-		return temp;
-	}
+String_t &String_t::prepend(const char *raw)
+{
+	appendAndSet(raw, string);
+	return *this;
+}
 
-	String_t& String_t::prepend(const char *raw)
-	{
-		appendAndSet(raw, string);
-		return *this;
-	}
+String_t &String_t::prepend(const String_t &st)
+{
+	return prepend(st.string);
+}
 
-	String_t& String_t::prepend(const String_t &st)
-	{
-		return prepend(st.string);
-	}
+String_t &String_t::operator+=(const char *raw)
+{
+	appendAndSet(string, raw);
+	return *this;
+}
 
-	String_t& String_t::operator+=(const char *raw)
-	{
-		appendAndSet(string, raw);
-		return *this;
-	}
+String_t &String_t::operator+=(const String_t &st)
+{
+	return *this += st.string;
+}
 
-	String_t& String_t::operator+=(const String_t &st)
-	{
-		return *this += st.string;
-	}
+bool String_t::operator<(const String_t &st) const
+{
+	return compareString(st) == 1;
+}
 
-	bool String_t::operator<(const String_t &st) const
-	{
-		return compareString(st) == 1;
-	}
+bool String_t::operator>(const String_t &st) const
+{
+	return compareString(st) == 2;
+}
 
-	bool String_t::operator>(const String_t &st) const
-	{
-		return compareString(st) == 2;
-	}
+bool String_t::operator<=(const String_t &st) const
+{
+	return compareString(st) != 2;
+}
 
-	bool String_t::operator<=(const String_t &st) const
-	{
-		return compareString(st) != 2;
-	}
+bool String_t::operator>=(const String_t &st) const
+{
+	return compareString(st) != 1;
+}
 
-	bool String_t::operator>=(const String_t &st) const
-	{
-		return mn0.compareString(st) != 1;
-	}
+bool String_t::operator==(const String_t &st) const
+{
+	return compareString(st) == 0;
+}
 
-	bool String_t::operator==(const String_t &st) const
-	{
-		return compareString(st) == 0;
-	}
+bool String_t::operator!=(const String_t &st) const
+{
+	return compareString(st) != 0;
+}
 
-	bool String_t::operator!=(const String_t &st) const
-	{
-		return compareString(st) != 0;
-	}
+char &String_t::operator[](size_t index)
+{
+	return index >= length ? string[length] : string[index];
+}
 
-	char &String_t::operator[](size_t index)
-	{
-		return index >= length ? string[length] : string[index];
-	}
+char String_t::operator[](size_t index) const
+{
+	return index >= length ? 0 : string[index];
+}
 
-	char String_t::operator[](size_t index) const
-	{
-		return index >= length ? 0 : string[index];
-	}
+bool String_t::contains(const String_t &st) const
+{
+	return strstr(string, st.string);
+}
 
-	bool String_t::contains(const String_t &st) const
-	{
-		return strstr(string, st.string);
-	}
+std::ostream &operator<<(std::ostream &os, const String_t &st)
+{
+	os << st.getString();
+	return os;
+}
 
-	std::ostream &operator<<(std::ostream &os, const String_t &st)
-	{
-		os << st.getString();
-		return os;
-	}
+void String_t::printString() const
+{
+	std::cout << *this;
+}
 
-	void String_t::printString() const
-	{
-		std::cout << *this;
-	}
+int String_t::firstOcc(char c) const
+{
+	char* res = strchr(string, c);
+	return res ? res - string : -1;
+}
 
-	size_t firstOcc(char c) const{
-		size_t res;
-		return res = strchr(string, c) ? res-string : -1;
-	}
+int String_t::lastOcc(char c) const
+{
+	char* res = strrchr(string, c);
+	return res ? res - string : -1;
+}
 
-	char *String_t::createEmpty()
-	{
-		char *s = new char[defaultCapacity];
-		s[0] = '\0';
-		return s;
-	}
+char *String_t::createEmpty()
+{
+	char *s = new char[defaultCapacity];
+	s[0] = '\0';
+	return s;
+}
 
-	//allocates a new string from a given raw
-	//result to be assigned to s1.string
-	//NO OBJECT CREATED!!!
-	char *String_t::createFrom(const char *s)
-	{
-		if (!s)
-			return createEmpty();
-		char *temp = new char[find2power(strlen(s) + 1)];
-		strcpy(temp, s);
-		return temp;
-	}
+//allocates a new string from a given raw
+//result to be assigned to s1.string
+//NO OBJECT CREATED!!!
+char *String_t::createFrom(const char *s)
+{
+	if (!s)
+		return createEmpty();
+	char *temp = new char[find2power(strlen(s) + 1)];
+	strcpy(temp, s);
+	return temp;
+}
 
-	int String_t::find2power(int num)
-	{
-		int base2 = 1;
-		while (base2 < num)
-			base2 *= 2;
-		return base2;
-	}
+unsigned int String_t::find2power(unsigned int num)
+{
+	unsigned int base2 = 1;
+	while (base2 < num)
+		base2 *= 2;
+	return base2;
+}
--- C++/string_t/string_t.h
+++ C++/string_t/string_t.h
@@ -12,24 +12,26 @@ public:
 	String_t(const String_t& st);
 	
 	String_t& operator= (const String_t& st);
-	inline bool operator< (const String_t& st) const;
-	inline bool operator> (const String_t& st) const;
-	inline bool operator<= (const String_t& st) const;
-	inline bool operator>= (const String_t& st) const;
-	inline bool operator== (const String_t& st) const;
-	inline bool operator!= (const String_t& st) const;
+	bool operator< (const String_t& st) const;
+	bool operator> (const String_t& st) const;
+	bool operator<= (const String_t& st) const;
+	bool operator>= (const String_t& st) const;
+	bool operator== (const String_t& st) const;
+	bool operator!= (const String_t& st) const;
 	String_t& operator+= (const String_t& st);
 	String_t& operator+= (const char* raw);
 	char& operator[] (size_t index);
 	char operator[] (size_t index) const;
 	
 
-	inline int getLength() const;
-	inline const char* getString() const;
-	inline static int getNumOfStrings();
-	inline static int getCaseSens();
-	inline static void setCaseSens(int flag);
-	inline void setString(const char* s);
+	inline size_t          getLength() const;
+	inline const char*     getString() const;
+	inline static int      getNumOfStrings();
+	inline static int      getCaseSens();
+	inline static size_t   getDefaultCapacity();
+	inline static void     setCaseSens(int flag);
+	inline static void     setDefaultCapacity(size_t capacity);
+	void                   setString(const char* s);
 	
 	int compareString(const String_t& st) const;
 	void printString() const;
@@ -38,40 +40,31 @@ public:
 	String_t& prepend(const String_t& st);
 	String_t& prepend(const char* raw);
 	bool contains(const String_t& st) const;
-	size_t firstOcc(char c) const;
-	size_t lastOcc(char c) const;
+	int firstOcc(char c) const;
+	int lastOcc(char c) const;
 
 private:
 	char* string;
-	unsigned int length;
-	int capacity;
+	size_t length;
+	size_t capacity;
 
 	static int numOfStrings;
 	static int caseSens;
-	static int defaultCapacity;
+	static size_t defaultCapacity;
 
 	char* createFrom(const char* s);
 	char* createEmpty();
 	void appendAndSet(const char *s1, const char *s2);
-	static int find2power(int num);
+	static unsigned int find2power(unsigned int num);
 };
 
-inline int getLength() const{
-	return length;
-}
-inline const char* getString() const{
-	return string;
-}
-inline static int getNumOfStrings(){
-	return numOfStrings;
-}
-inline static int getCaseSens(){
-	return caseSens;
-}
-inline static void setCaseSens(int flag){
-	caseSens = flag;
-}
-
+inline size_t String_t::getLength() const{ return length;}
+inline const char* String_t::getString() const{ return string;}
+inline  int String_t::getNumOfStrings(){ return numOfStrings;}
+inline  int String_t::getCaseSens(){ return caseSens;}
+inline  void String_t::setCaseSens(int flag){ caseSens = flag;}
+inline  size_t  String_t::getDefaultCapacity(){ return defaultCapacity;}
+inline  void String_t::setDefaultCapacity(size_t cap){ defaultCapacity = cap;}
 
 std::ostream& operator<<(std::ostream& os, const String_t& st);
 
