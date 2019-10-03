233e4262c9f7256f2580a45fb7da64870d14c900
RamyLerner <san85@walla.com>
2019-10-03 15:52:43 +0300

    improved test

--- C++/City/cityTest.cpp
+++ C++/City/cityTest.cpp
@@ -2,12 +2,21 @@
 #include "street_t.h"
 #include "city_t.h"
 #include <string>
+#include <iostream>
 
 int main(){
 	Building_t<string> bd;
 	Street_t<int, int> st;
 	City_t<string, int, string> ct;
+
+	bd.setBuildingID("build");
+	st.setStreetID(8);
+	ct.setCityID("NATANYA");
 	
+	std::cout << bd.getBuildingID() << endl;
+	std::cout << st.getStreetID() << endl;
+	std::cout << ct.getCityID() << endl;
+
 	return 0;
 	
 }
