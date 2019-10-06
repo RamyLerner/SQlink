d959f2f81cb23758c3d1daf4a7749441abb55133
RamyLerner <san85@walla.com>
2019-10-06 13:04:57 +0300

    works

--- C++/ExceptionHandler/tException_t.cpp
+++ C++/ExceptionHandler/tException_t.cpp
@@ -1,20 +1,28 @@
-#include <string>
 #include "tException_t.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
-template <class T>
-TException_t<T>::TException_t(const T& excep, const std::string& message, const char* fileName, int lineNumber): 
-	m_excep(excep), m_message(message), m_fileName(fileName), m_line(lineNumber){}
-		
-template <class T>
-TException_t<T>::TException_t(const TException_t& excep):
-	m_excep(excep.m_excep), m_message(excep.m_message), m_fileName(excep.m_fileName), m_line(excep.m_line){}
-	
-template <class T>
-TException_t<T>& TException_t<T>::operator=(const TException_t<T>& excep){
-	if (*this != excep){
-		m_excep = excep.m_excep;
-		m_message = excep.m_message;
-		m_fileName = excep.m_fileName;
-		m_line = excep.m_line;
+int getIndex(){
+	int i;
+	cout << "enter elemnt index to retrieve from arr[6]" << endl;
+	cin >> i;
+	if (i >= 6)
+		throw TException_t<int>(i, "Index too big", __FILE__, __LINE__);
+	if (i < 0)
+		throw TException_t<std::string>("oops", "Negative Index", __FILE__, __LINE__);
+	return i;
+}
+
+int main(){
+	int arr[6] = {0};
+	try {
+		cout << arr[getIndex()];
+	}
+	catch (const TException_t<int>& excep){
+		excep.print();
+	}
+	catch (const TException_t<std::string>& excep){
+		excep.print();
 	}
 }
--- C++/ExceptionHandler/tException_t.h
+++ C++/ExceptionHandler/tException_t.h
@@ -1,47 +1,58 @@
 #ifndef TEXCEPTION_H
 #define TEXCEPTION_H
 
-#include<string>
-#include<iostream>
-#define THROW_TEXCEPTION(obj,msg) throw TException_t(obj, msg, __FILE__, __LINE__)
+#include <string>
+#include <iostream>
 
-	template <class T>
-	class TException_t{
-		public:
-			~TException_t();
-			TException_t(){}
-			TException_t(const T& excep, const std::string& message, const char* fileName, int lineNumber);
-			TException_t(const TException_t& texcp);
-			TException_t& operator=(const TException_t& texcep);
-			inline void print() const;
-			
-			inline const T           getException()  const;
-			inline const std::string getMessage()    const;
-			inline const char*       getFileName()   const;
-			inline       int         getLineNumber() const;
-		
-		private:
-			T m_excep;
-			std::string m_message;
-			char* m_fileName;
-			int m_line;
-	};
-	
-	template <class T>
-	inline const T TException_t<T>::getException() const { return m_excep;}
-	
-	template <class T>
-	inline const std::string TException_t<T>::getMessage() const { return m_message;}
-	
-	template <class T>
-	inline const char* TException_t<T>::getFileName() const {return m_fileName;}
-	
-	template <class T>
-	inline int TException_t<T>::getLineNumber() const {return m_line;}
-		
-	template <class T>
-	inline void TException_t<T>::print() const {
-		std::cout << "EXCEPTION: in " << m_fileName << "(" << m_line << ") " << m_message << ": " << m_excep << std::endl;
-	}
+template <class T>
+class TException_t
+{
+public:
+	~TException_t() {}
+	TException_t() {}
+	TException_t(const T &excep, const std::string &message,  const std::string& fileName, int lineNumber);
+	TException_t(const TException_t &texcp);
+
+	inline void print() const;
+
+	inline const T &getException() const;
+	inline const std::string &getMessage() const;
+	inline const char *getFileName() const;
+	inline int getLineNumber() const;
+
+private:
+	T m_excep;
+	std::string m_message;
+	std::string m_fileName;
+	int m_line;
+
+	TException_t &operator=(const TException_t &texcep) {}
+};
+
+template <class T>
+TException_t<T>::TException_t(const T &excep, const std::string &message,  const std::string& fileName, int lineNumber) : 
+	m_excep(excep), m_message(message), m_fileName(fileName), m_line(lineNumber) {}
+
+template <class T>
+TException_t<T>::TException_t(const TException_t &excep) : 
+	m_excep(excep.m_excep), m_message(excep.m_message), m_fileName(excep.m_fileName), m_line(excep.m_line) {}
+
+template <class T>
+inline const T &TException_t<T>::getException() const { return m_excep; }
+
+template <class T>
+inline const std::string &TException_t<T>::getMessage() const { return m_message; }
+
+template <class T>
+inline const char *TException_t<T>::getFileName() const { return m_fileName; }
+
+template <class T>
+inline int TException_t<T>::getLineNumber() const { return m_line; }
+
+template <class T>
+inline void TException_t<T>::print() const
+{
+	std::cout << "EXCEPTION: in " << m_fileName << "(" << m_line << ") " << m_message << ": " << m_excep << std::endl;
+}
 
 #endif
