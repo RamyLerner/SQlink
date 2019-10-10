f265010f3f69cca3373d4f9d6792594eeb4e3a26
RamyLerner <san85@walla.com>
2019-10-10 04:47:04 +0300

    yet to complete

--- C++/tcontainer/tContainer_t.cpp
+++ C++/tcontainer/tContainer_t.cpp
@@ -1,4 +1,17 @@
 #include "tContainer_t.h"
 #include <vector>
 #include <list>
+#include <iostream>
+
+int main(){
+    tContainer_t<int, std::vector> vecC;
+   
+
+    vecC.push_back(5);
+    vecC.push_back(9);
+    vecC.push_back(3);
+    vecC.push_back(4);
+    vecC.push_back(20);
+    std::cout << vecC[3] << std::endl;
+}
 
--- C++/tcontainer/tContainer_t.h
+++ C++/tcontainer/tContainer_t.h
@@ -2,11 +2,14 @@
 #define TCONTAINER
 
 #include<stdlib.h>
+#include<algorithm>
+#include<typeinfo>
+#include<vector>
 
-    template <class T, class C>
+    template <class T, template <typename> class C>
     class tContainer_t{
-        typedef typename C::iterator iter_t;
-        typedef typename C::const_iterator citer_t;
+        typedef typename C<T>::iterator iter_t;
+        typedef typename C<T>::const_iterator citer_t;
 
         public:
             ~tContainer_t();
@@ -20,27 +23,97 @@
             T* removeElem (const T& elem); //should call erase()
             void clear(); //removes all elements (and destroying) without deleting!!
             void remove_destroy (T& elem);
-            void remove_destroy();
+            void clear_destroy();
             T* operator[](size_t index);
 
         private:
-            C container;
+            C<T> container;
             tContainer_t(const tContainer_t& );
             tContainer_t& operator=(const tContainer_t&);
     };
-template <class T, class C>
+
+    template <class T>
+    class comparison{
+        public:
+            comparison(T t) : val(t) {}
+            bool operator()(const T* param) const {return *param == val;}
+        private:
+            T val;
+    };
+
+template  <class T, template <typename> class C>
+tContainer_t<T, C>::tContainer_t() { container = new C<T>;}
+
+template  <class T, template <typename> class C>
+tContainer_t<T, C>::~tContainer_t() { delete container;}
+
+template  <class T, template <typename> class C>
 inline size_t tContainer_t<T, C>::size() const { return container.size();}
 
-template <class T, class C>
+template  <class T, template <typename> class C>
 inline bool tContainer_t<T, C>::empty() const { return container.empty();}
 
-template <class T, class C>
+template  <class T, template <typename> class C>
 inline void tContainer_t<T, C>::push_back(const T* elem) { container.push_back(elem);}
 
-template <class T, class C>
+template  <class T, template <typename> class C>
 inline const T* tContainer_t<T, C>::front() const { return container.front();}
 
-template <class T, class C>
+template  <class T, template <typename> class C>
 inline const T* tContainer_t<T, C>::back() const { return container.back();}
+
+template  <class T, template <typename> class C>
+const T* tContainer_t<T, C>::findElement(const T& elem) const {
+    citer_t it = std::find_if(container.begin(), container.end(), comparison<T>(elem));
+    return *it;
+}
+
+template  <class T, template <typename> class C>
+T* tContainer_t<T, C>::removeElem (const T& elem){
+    iter_t it = std::find_if(container.begin(), container.end(), comparison<T>(elem));
+    T* tPtr = *it;
+    container.erase(it);
+    return tPtr;
+}
+
+template  <class T, template <typename> class C>
+void tContainer_t<T, C>::clear(){ container.clear();}
+
+template  <class T, template <typename> class C>
+void tContainer_t<T, C>::remove_destroy (T& elem){
+    iter_t it = std::find_if(container.begin(), container.end(), comparison<T>(elem));
+    delete *it;
+    container.erase(it);
+}
+
+template  <class T, template <typename> class C>          
+void tContainer_t<T, C>::clear_destroy(){
+    citer_t it;
+    for (it = container.begin(); it != container.end(); ++it){
+        delete *it;
+    }
+    container.clear();
+}
+
+template  <class T, template <typename> class C>     
+T* tContainer_t<T, C>::operator[](size_t index){
+    int i=0;
+    if (typeid(container) == typeid(std::vector<T>))
+        try {
+            return container[index];
+        }
+        catch (std::bad_alloc&){
+            throw;
+        }
+    else
+    {
+        if (index >= container.size())
+            throw std::bad_alloc();
+        citer_t it = container.begin();
+        for (i=0; i<index; i++)
+            it++;
+        return *it;
+    }
+}
 
 #endif
